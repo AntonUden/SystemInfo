a7990543b62a4ebb1d7f69522fde925879f9a3cf
BuildTools <udenanton@hotmail.com>
2018-05-14 12:35:38 +0200

    First version

--- SystemInfo/Program.cs
+++ SystemInfo/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.Drawing;
 
 namespace SystemInfo
 {
@@ -10,6 +13,25 @@ namespace SystemInfo
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("OS: " + Environment.OSVersion);
+            Console.WriteLine("64 bit: " + Environment.Is64BitOperatingSystem);
+            Console.WriteLine("Processor count: " + Environment.ProcessorCount);
+
+            Console.WriteLine("Machine name: " + Environment.MachineName);
+            Console.WriteLine("Computer name: " + SystemInformation.ComputerName);
+            Console.WriteLine("User name: " + SystemInformation.UserName);
+            Console.WriteLine("User domain name: " + SystemInformation.UserDomainName);
+
+            Console.WriteLine("Mouse wheel present: " + SystemInformation.MouseWheelPresent);
+            Console.WriteLine("Mouse buttons: " + SystemInformation.MouseButtons);
+
+            Console.WriteLine("Network: " + SystemInformation.Network);
+
+            Console.WriteLine("Working area:  Width: " + SystemInformation.WorkingArea.Width + " Height: " + SystemInformation.WorkingArea.Height);
+            Console.WriteLine("Monitor count: " + SystemInformation.MonitorCount);
+            Console.WriteLine("Primary monitor size: " + SystemInformation.PrimaryMonitorSize.Width + "x" + SystemInformation.PrimaryMonitorSize.Height);
+
+            Console.ReadKey();
         }
     }
 }
